1104a7b43673411430b88ce6966067f3f94f5e22
Brandon Holcombe <bholcombe@trifinlabs.com>
2019-02-26 09:59:44 -0800

    First commit

---
+++ config.rb
@@ -0,0 +1,10 @@
+preferred_syntax = :scss
+
+css_dir = "assets/css" # by Compass.app
+sass_dir = "assets/scss" # by Compass.app
+images_dir = "assets/img" # by Compass.app
+output_style = :compressed # by Compass.app
+relative_assets = true # by Compass.app
+line_comments = false # by Compass.app
+sass_options = {:debug_info=>false} # by Compass.app
+sourcemap = false # by Compass.app
